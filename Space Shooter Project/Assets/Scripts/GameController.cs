14a2d8109049d2da4fe082609f108402ebf1d357
LBentes <60331057+LBentes@users.noreply.github.com>
2020-04-19 21:18:18 -0400

    Final

---
+++ Space Shooter Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+    public AudioSource musicSource2;
+    public AudioSource musicSource3;
+
+    public Text ScoreText;
+    public Text restartText;
+    public Text gameOverText;
+    public Text winText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                SceneManager.LoadScene("Main");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'T' to Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            winText.text = "You Win! Game created by Leonardo Bentes";
+            gameOver = true;
+            restart = true;
+            musicSource2.clip = musicClipTwo;
+            musicSource2.Play();
+            musicSource3.Stop();
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over! Game created by Leonardo Bentes";
+        gameOver = true;
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource3.Stop();
+    }
+}
